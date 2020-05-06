4dd072b7ce5af583dc6d0f2e1b5db0eac1270892
Poenomaly <djbenson741@gmail.com>
2020-05-06 00:22:07 -0600

    Add my_mod and my_script files

--- lambdata_poe/my_mod.py
+++ lambdata_poe/my_mod.py
@@ -2,7 +2,18 @@
 
 
 def enlarge(n):
-    return n * 100
+    return int(n) * 100
 
-x = 5
-print(enlarge(x))
+if __name__ == "__main__":
+    # Only run the following code
+    # when we run this script from
+    # the command-line
+    # Otherwise don't invoke this 
+    # code (for example when import
+    # from another file)
+
+    x = 5
+    print(enlarge(x))
+
+    z = input("Please choose a number to enlarge (ex. 4): ")
+    print(enlarge(int(z)))
--- lambdata_poe/my_script.py
+++ lambdata_poe/my_script.py
@@ -1,8 +1,13 @@
 import pandas
 
+from lambdata_poe.my_mod import enlarge
+
 print("HELLO WORLD!")
 
 print(2+2)
 
 df = pandas.DataFrame({"a":[1,2,3], "b":[4,5,6]})
 print(df.head())
+
+y = 9
+print(enlarge(y))
